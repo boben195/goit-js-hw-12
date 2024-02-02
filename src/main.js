5741ed44de3d0cf618deb457e410695b79e3677b
boben195 <boben195@ukr.net>
2024-02-02 16:58:38 +0200

    add js problem with axios

--- src/main.js
+++ src/main.js
@@ -12,82 +12,93 @@ const form = document.querySelector(".foto-form");
 const list = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const query = form.query.value.trim();
-  if (!query) {
-    createMessage(
-      `The search field can't be empty! Please, enter your request!`
-    );
-    return;
-  }
-  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+//************************ */
+let page = 1;
+let query = "";
+let total = 0;
 
-  getPictures(url)
-    .then(data => {
-      if (data.hits.length === 0) {
-        createMessage(
-          `Sorry, there are no images matching your search query. Please, try again!`
-        );
-        showLoader(false);
-      }
+form.addEventListener('submit', handleSearch);
 
-      list.innerHTML = createList(data.hits);
-      showLoader(false);
-      const simplyGallery = new SimpleLightbox('.gallery-item a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      form.reset();
-      simplyGallery.refresh();
-    })
-    .catch(error => {
-      iziToast.error({
-        title: 'ERROR',
-        message: `❌ Ooopsi Doopsi ${error}`,
-      });
-    });
+
+
+
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const query = form.query.value.trim();
+//   if (!query) {
+//     createMessage(
+//       `The search field can't be empty! Please, enter your request!`
+//     );
+//     return;
+//   }
+//   const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+//   getPictures(url)
+//     .then(data => {
+//       if (data.hits.length === 0) {
+//         createMessage(
+//           `Sorry, there are no images matching your search query. Please, try again!`
+//         );
+//         showLoader(false);
+//       }
+
+//       list.innerHTML = createList(data.hits);
+//       showLoader(false);
+//       const simplyGallery = new SimpleLightbox('.gallery-item a', {
+//         captionsData: 'alt',
+//         captionDelay: 250,
+//       });
+//       form.reset();
+//       simplyGallery.refresh();
+//     })
+//     .catch(error => {
+//       iziToast.error({
+//         title: 'ERROR',
+//         message: `❌ Ooopsi Doopsi ${error}`,
+//       });
+//     });
   
-});
+// });
 
-function getPictures(url) {
-  showLoader(true);
-  return fetch(url).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
-}
+// function getPictures(url) {
+//   showLoader(true);
+//   return fetch(url).then(resp => {
+//     if (!resp.ok) {
+//       throw new Error(resp.statusText);
+//     }
+//     return resp.json();
+//   });
+// }
 
-function createList(hits) {
-  return hits
-    .map(
-      ({
-        webformatURL, largeImageURL, tags, likes, views, comments, downloads, }) =>
-        `<li class="gallery-item">
-        <a class="gallery-link" href="${largeImageURL}">
-        <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
-        <p class="gallery-text">Likes: ${likes} Views: ${views} Comments: ${comments} Downloads: ${downloads}</p>
-        </a>
-        </li>`)
-    .join('');
-    simplyGallery.refresh();
-}
+// function createList(hits) {
+//   return hits
+//     .map(
+//       ({
+//         webformatURL, largeImageURL, tags, likes, views, comments, downloads, }) =>
+//         `<li class="gallery-item">
+//         <a class="gallery-link" href="${largeImageURL}">
+//         <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
+//         <p class="gallery-text">Likes: ${likes} Views: ${views} Comments: ${comments} Downloads: ${downloads}</p>
+//         </a>
+//         </li>`)
+//     .join('');
+//     simplyGallery.refresh();
+// }
 
 
-function createMessage(message) {
-  iziToast.show({
-    message: message,
-    close: false,
-    closeOnClick: true,
-  });
-}
+// function createMessage(message) {
+//   iziToast.show({
+//     message: message,
+//     close: false,
+//     closeOnClick: true,
+//   });
+// }
 
-function showLoader(state = true) {
-  loader.classList.add('loader')
-  loader.style.display = !state ? 'none' : 'inline-block';
-  btn.disabled = state;
-}
+// function showLoader(state = true) {
+//   loader.classList.add('loader')
+//   loader.style.display = !state ? 'none' : 'inline-block';
+//   btn.disabled = state;
+// }
 
-loader.classList.remove('loader')
+// loader.classList.remove('loader')
